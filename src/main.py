eefc471b5a203ef8c948470af8473a1991b4c0b8
Nevin Toms <nevintoms333@gmail.com>
2024-06-22 18:30:29 -0400

    Add main file

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from controller import RobotController
+
+from random import randint
+
+
+def get_live_robot_cords():
+    # get the current position of the robot using
+    # the geolocation device.
+    # call to robot api/sdk for live co-ordinates
+    return (randint(0, 10), randint(0, 10))
+
+
+if __name__ == "__main__":
+    rc = RobotController(get_live_robot_cords)
+    print("-------START-------")
+    rc.start()
+    print("-------END-------")
