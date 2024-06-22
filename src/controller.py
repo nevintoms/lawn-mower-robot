4bb90525dcdd9246ddf6f0c0c1ee004168332a60
Nevin Toms <nevintoms333@gmail.com>
2024-06-22 18:30:06 -0400

    Add RobotController class

---
+++ src/controller.py
@@ -0,0 +1,20 @@
+from location import Map
+from robot import Robot
+
+class RobotController:
+
+    def __init__(self, get_live_robot_cords) -> None:
+        self.map = Map()
+        self.robot = Robot(get_live_robot_cords)
+
+    def run_robot(self, zigzag_path):
+        for cords in zigzag_path:
+            start, end = cords
+            self.robot.move(end)
+
+    def start(self):
+        zigzag_path = self.map.get_path()
+        start_cords = zigzag_path[0][0]
+        # move robot to start cords
+        self.robot.move(start_cords)
+        self.run_robot(zigzag_path)
