4701fa9fbd45a1a5da0c0fd7e0953d22a9aa3f49
Nevin Toms <nevintoms333@gmail.com>
2024-06-23 15:41:50 -0400

    Add visualise

--- src/controller.py
+++ src/controller.py
@@ -1,11 +1,13 @@
 from location import Map
 from robot import Robot
+from visualise import Visualise
 
 class RobotController:
 
     def __init__(self, get_live_robot_cords) -> None:
         self.map = Map()
         self.robot = Robot(get_live_robot_cords)
+        self.viz = Visualise()
 
     def run_robot(self, zigzag_path):
         for cords in zigzag_path:
@@ -13,8 +15,10 @@ class RobotController:
             self.robot.move(end)
 
     def start(self):
-        zigzag_path = self.map.get_path()
-        start_cords = zigzag_path[0][0]
-        # move robot to start cords
-        self.robot.move(start_cords)
-        self.run_robot(zigzag_path)
+        zigzag_path, cords = self.map.get_path_with_cords()
+        self.viz.plot(zigzag_path, cords)
+        
+        # start_cords = zigzag_path[0][0]
+        # # move robot to start cords
+        # self.robot.move(start_cords)
+        # self.run_robot(zigzag_path)
