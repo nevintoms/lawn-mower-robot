00c86ee59c19e4d9501e3e008e9feafe5f44c27a
Nevin Toms <nevintoms333@gmail.com>
2024-06-24 10:44:32 -0400

    Add animation to the plot

--- src/visualise.py
+++ src/visualise.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
+import matplotlib.animation as animation
 
 
 class Visualise:
@@ -11,21 +12,36 @@ class Visualise:
         polygon = Polygon(cords)
         x, y = polygon.exterior.xy
 
-        # Plot the polygon
-        plt.figure()
-        plt.plot(x, y, "lightgreen")
-        plt.fill(x, y, "lightgreen")
+        # Create the figure and axis
+        fig, ax = plt.subplots()
+        ax.plot(x, y, "lightgreen")
+        ax.fill(x, y, "lightgreen")
 
-        # Plot the zigzag path
-        if zigzag_path:
-            path_x, path_y = zip(*zigzag_path)
-            plt.plot(path_x, path_y, "red")
+        # Initialize the plot with empty lines
+        (path_line,) = ax.plot([], [], "red")
 
         # Set plot limits and aspect ratio
-        # plt.xlim(min(x)-0.0001, max(x)+0.0001)
-        # plt.ylim(min(y)-0.0001, max(y)+0.0001)
-        # plt.gca().set_aspect('equal', adjustable='box')
-        plt.axis("off")
+        # ax.set_xlim(min(x) - 0.0001, max(x) + 0.0001)
+        # ax.set_ylim(min(y) - 0.0001, max(y) + 0.0001)
+        # ax.set_aspect('equal', adjustable='box')
+        ax.axis("off")
+
+        # Animation update function
+        def update(frame):
+            if frame == 0:
+                return (path_line,)  # Nothing to plot in the first frame
+
+            # Select points up to the current frame
+            path_x, path_y = zip(*zigzag_path[: frame + 1])
+
+            # Update the line data
+            path_line.set_data(path_x, path_y)
+            return (path_line,)
+
+        # Create the animation
+        ani = animation.FuncAnimation(
+            fig, update, frames=len(zigzag_path), interval=100, blit=True
+        )
 
         # Show the plot
         plt.show()
